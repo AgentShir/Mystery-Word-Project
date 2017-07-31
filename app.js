08340cc8f4e6f9d1a1f4e10515a9071eb1f007b0
Shireen Garcia <garciasal@gmail.com>
2017-07-31 13:50:06 -0700

    Added in body parser and got Guesses to show up properly

--- app.js
+++ app.js
@@ -2,18 +2,46 @@ const express = require('express')
 const app = express()
 const path = require('path')
 const mustacheExpress = require('mustache-express');
+const bodyParser = require('body-parser');
+
 // const words = fs.readFileSync("/usr/share/dict/words", "utf-8").toLowerCase().split("\n");
 
 
 app.engine('mustache', mustacheExpress());
 app.set('views', './views')
 app.set('view engine', 'mustache')
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
 
 app.use(express.static(path.join(__dirname, 'static')))
 
 app.get("/", function(req, res, next){
-  res.render("index", {appType:"Express"})
+  res.render("index")
 })
+
+var users = [];
+
+app.post('/', function(req, res){
+  console.log('Guesses: ', req.body.guess);
+  res.render("guess", {guess:req.body.guess});
+});
+
+app.use(express.static(path.join(__dirname, 'static')))
+
+app.get("/", function(req, res, next){
+
+  res.render("index");
+
+});
+
+// var users = [];
+//
+// app.post('/', function(req, res){
+//   console.log('BODY: ', req.body);
+//   res.render("user", {email:req.body.email, name:req.body.name, year:req.body.year, position:req.body.position, password:req.body.password});
+// });
+
 
 app.listen(3000, function(){
   console.log("App running on port 3000")
